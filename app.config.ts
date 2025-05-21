4291c77e68bf5ca18c76438398097e15061ca2df
Raulif <raul4cade@gmail.com>
2025-05-21 10:40:16 +0200

    Uncomment code

--- app.config.ts
+++ app.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
-      // copyAppCssToPublic()
+      copyAppCssToPublic()
     ],
   },
 });
